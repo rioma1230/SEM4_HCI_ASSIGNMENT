f3203adebc6c1616fb6ef337942f8446a441702e
Tsang Chun Hei <180432474@stu.vtc.edu.hk>
2019-10-19 01:39:09 +0800

    initial commit

--- js/findmenu.js
+++ js/findmenu.js
@@ -1,41 +1,95 @@
 var data = [{
-    "id": 198,
-    "name": "Aaron Garo",
-}, {
-    "id": 345,
-    "name": "Michael Stines",
-}, {
-    "id": 545,
-    "name": "Ully Heiz",
-}, {
-    "id": 678,
-    "name": "Asgaf Torino",
-}]
+        "name": "FOOD",
+        "dishes": {
+            "dname": "FOOD",
+            "price": 15.5
+        },
+        "is_enabled": true,
+        "is_special": true,
+        "from": "2012-04-21T18:25:43-05:00",
+        "to": "2020-04-21T18:25:43-05:00"
+    },
+    {
+        "name": "SomethingGoodeat",
+        "dishes": {
+            "dname": "GOOD",
+            "price": 3000
+        },
+        "is_enable": true,
+        "is_special": true,
+        "from": "2019-04-21T18:25:43-05:00",
+        "to": "2020-04-21T18:25:43-05:00"
+    },
+    {
+        "name": "FOOD1",
+        "dishes": {
+            "dname": "GOOD",
+            "price": 3000
+        },
+        "is_enable": true,
+        "is_special": true,
+        "from": "2012-04-21T18:25:43-05:00",
+        "to": "2020-04-21T18:25:43-05:00"
+    },
+    {
+        "name": "FOOD2",
+        "dishes": {
+            "dname": "dsa",
+            "price": 12
+        },
+        "is_enable": true,
+        "is_special": true,
+        "from": "2012-04-21T18:25:43-05:00",
+        "to": "2020-04-21T18:25:43-05:00"
+    },
+    {
+        "name": "FOOD3",
+        "dishes": {
+            "dname": "GOOD",
+            "price": 3000
+        },
+        "is_enable": true,
+        "is_special": true,
+        "from": "2012-04-21T18:25:43-05:00",
+        "to": "2020-04-21T18:25:43-05:00"
+    }
+];
 
 output = "";
 $.each(data, function(key, val) {
-    output += "<div class='values'>";
-    output += '<h5 class="value-id">' + val.id + '</h5>';
-    output += '<p class="value-name">' + val.name + '</p>'
-    output += "</div>";
-});
 
-$('#content').html(output);
+    var tr = document.createElement("tr");
+
+    var tdName = document.createElement("td");
+    tdName.innerHTML = val.name;
+
+    var tdDname = document.createElement("td");
+    tdDname.innerHTML = val.dishes.dname;
+
+    var tdPrice = document.createElement("td");
+    tdPrice.innerHTML = val.dishes.price;
+
+    tr.append(tdName);
+    tr.append(tdDname);
+    tr.append(tdPrice);
+    $('#jsonTable').append(tr);
+
+});
 
 /* SEEKER FUNCTION */
 if (!RegExp.escape) {
     RegExp.escape = function(s) {
-        return s.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&")
+        return s.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
     };
 }
 
 jQuery(function() {
     var $rows = $('.values');
     $('#seeker').keyup(function() {
-        var regex = new RegExp(RegExp.escape($.trim(this.value).replace(/\s+/g, ' ')), 'i')
+        var regex = new RegExp(RegExp.escape($.trim(this.value).replace(/\s+/g, ' ')), 'i');
         $rows.hide().filter(function() {
             var text = $(this).children(".value-name").text().replace(/\s+/g, ' ');
-            return regex.test(text)
+            return regex.test(text);
         }).show();
     });
 });
