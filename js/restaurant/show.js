943a2fe4698d5110c837806e22a3df5b14d90ac4
JohnDoeAntler <yosuganosoraemailtest@gmail.com>
2019-11-28 22:24:42 +0800

    like and favourite function

--- js/restaurant/show.js
+++ js/restaurant/show.js
@@ -18,3 +18,29 @@ if (Cookies.get('role')){
 		$('#should-login').remove();
 	}
 } 
+
+var liked = false, favourited = false
+var likes = 10, favourites = 10
+
+$('#likes').text(likes)
+$('#favourites').text(favourites)
+
+$('#like-btn').click(function() {
+	if (liked) {
+		$('#likes').text(likes)
+		liked = false;
+	} else {
+		$('#likes').text(likes + 1)
+		liked = true;
+	}
+})
+
+$('#favourite-btn').click(function() {
+	if (favourited) {
+		$('#favourites').text(favourites)
+		favourited = false;
+	} else {
+		$('#favourites').text(favourites + 1)
+		favourited = true;
+	}
+})
